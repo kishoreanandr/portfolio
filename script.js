a83b0087e10bf2269bd2b8a1ab5f23a48903e5b1
Kishore Anand <kishoreanand.r@gmail.com>
2024-10-27 18:30:31 +0530

    Some minor changes

--- script.js
+++ script.js
@@ -15,24 +15,6 @@ let navlinks=document.querySelectorAll('header nav a');
 
 window.onscroll=()=>
     {
-        // sections.forEach(sec=>
-        //     {
-        //         let top=window.scrollY;
-        //         let offset=sec.offsetTop-150;
-        //         let height=sec.offsetHeight;
-        //         let id=sec.getAttribute('id');
-
-        //         if(top>=offset && top<offset+height)
-        //             {
-        //                 navlinks.forEach(links=>
-        //                     {
-        //                         links.classList.remove('active');
-        //                         document.querySelector('header nav a[href*='+id+']').classList.add('active');
-        //                     }
-        //                 );
-        //             };
-        //     }
-        // );
 
         //sticky navbar
         let header=document.querySelector('header');
@@ -42,18 +24,6 @@ window.onscroll=()=>
         menuIcon.classList.remove('fa-xmark');
         navbar.classList.remove('active');
     };
-
-    // //scroll reveal
-    // ScrollReveal({ 
-    //     distance: '80px',
-    //     duration:2000,
-    //     delay:200,
-    //  });
-    // ScrollReveal().reveal('.home-content,heading', { origin:'top' });
-    // ScrollReveal().reveal('.home-img, .projects-container,.portfolio-box,.contact', { origin:'button'});
-    // ScrollReveal().reveal('.home-content h1,.about-img', { origin:'left' });    
-    // ScrollReveal().reveal('.home-contact p,.about-content', { origin:'right'});
-
 
 
 
